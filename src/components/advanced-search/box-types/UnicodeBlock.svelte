b3971ca9a5ac8a99cb519abd93ac5f61ad316d22
EmNudge <emnudge@gmail.com>
2021-01-17 13:50:34 -0500

    added alphabetic sort switch

--- src/components/advanced-search/box-types/UnicodeBlock.svelte
+++ src/components/advanced-search/box-types/UnicodeBlock.svelte
@@ -4,7 +4,14 @@
   import Dropdown from '../Dropdown.svelte';
   import { blockLookupStore } from '../../../stores';
 
-  const blockNames = $blockLookupStore.map(block => block.name).sort();
+  const blockNames = $blockLookupStore.map(block => block.name);
+
+  let shouldSort = true;
+  $: console.log({ shouldSort })
+  $: blocks = shouldSort 
+    ? [...blockNames].sort()
+    : blockNames;
+  
   let blockName: string = blockNames[0];
 
   $: {
@@ -13,6 +20,11 @@
 </script>
 
 <Dropdown 
-  options={blockNames} 
+  options={blocks} 
   hue={35} 
   bind:value={blockName} />
+
+<label>
+  <span>Sort Blocks Alphabetically</span>
+  <input type="checkbox" bind:checked={shouldSort} />
+</label>
