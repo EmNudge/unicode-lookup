cc102e42e4301b8a3eb400d894a7bc9743edd8fa
EmNudge <emnudge@gmail.com>
2021-02-06 20:17:04 -0500

    added glob match and range match to simple search

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -11,16 +11,35 @@ export function getBoxSetsFromText(text: string): BoxSet[] {
     return [query];
 }
 
-function getNumFromText(text: string): number {
+function getNumFromText(text: string): number | null {
     if (/^[0-9]+$/.test(text)) return Number(text);
     if (/^0x[0-9a-fA-F]+$/.test(text)) return Number(text);
     if (/^0b[10]+$/.test(text)) return parseInt(text.slice(2), 2);
     return null;
 }
 
+function getNumRangeFromText(text: string): [number, number] | null {
+    const numStrings = text.split('-');
+    if (numStrings.length !== 2) return null;
+
+    const n1 = getNumFromText(numStrings[0].trim());
+    if (!n1) return null;
+    const n2 = getNumFromText(numStrings[1].trim());
+    if (!n2) return null;
+
+    return [n1, n2];
+}
+function getRegexFromGlob(text: string): RegExp | null {
+    if (!text.includes('*')) return null;
+    if (!/^[a-zA-Z-* ]+$/.test(text)) return null;
+
+    const regexText = text.replace(/\*/g, '.*');
+    return new RegExp(regexText, 'i');
+}
+
 export function getQueryFromText(text: string): Box {
     const num = getNumFromText(text);
-    if (typeof num === "number") {
+    if (num !== null) {
         const char = String.fromCodePoint(num);
         return { 
             name: 'Is Near Char',
@@ -33,6 +52,26 @@ export function getQueryFromText(text: string): Box {
             name: 'Is Near Char',
             data: { char: text, distance: 0 }
         };
+    }
+
+    // allow ranges of codepoints using '-' as a separater
+    const range = getNumRangeFromText(text);
+    if (range) {
+        return {
+            name: 'Codepoint Range',
+            data: { from: range[0], to: range[1] },
+        }
+    }
+
+    const globRegex = getRegexFromGlob(text.trim());
+    if (globRegex) {
+        return {
+            name: 'Regex Match',
+            data: {
+                regex: globRegex,
+                matchOn: 'Name',
+            }
+        }
     }
 
     const regexRes = text.match(/\/(.+?)\/([a-z]+)?/);
