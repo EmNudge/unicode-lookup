b9a340ddeff7e6475d6152af865d91dd1cfb9459
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-01-25 10:15:00 -0800

    ignore lint

--- src/components/table-container/ResultsContainer.svelte
+++ src/components/table-container/ResultsContainer.svelte
@@ -53,6 +53,7 @@
 </p>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="table"
   on:click={handleClick} 
   on:contextmenu={handleRightClick} 
